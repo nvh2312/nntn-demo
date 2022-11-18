3ea6e505614c3759ef6761126b224529a2b0635a
nvh2312 <mdr3132003@gmail.com>
2022-11-18 17:44:36 +0700

    update a

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -26572,7 +26572,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60428" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56888" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,14 @@ const userController = require("./../controllers/userController");
 
 const router = express.Router();
 router.get("/", userController.getAllUsers);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 router
   .route("/:id")
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-router.get("/logout", authController.logout);
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-router.get("/logout", authController.logout);
+
 
 module.exports = router;
